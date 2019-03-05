48257946afbe52aa1bad56f151c6e9b4d3bad43b
Melvin Mallari <mtmallari@ucdavis.edu>
2019-03-05 10:25:28 -0800

    Add buying_power to users

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: stocks
+#
+#  id            :bigint(8)        not null, primary key
+#  ticker_symbol :string           not null
+#  company_name  :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Stock < ApplicationRecord
   validates :ticker_symbol, :company_name, presence: true
   validates :ticker_symbol, uniqueness: true
--- test/models/stock_test.rb
+++ test/models/stock_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: stocks
+#
+#  id            :bigint(8)        not null, primary key
+#  ticker_symbol :string           not null
+#  company_name  :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 require 'test_helper'
 
 class StockTest < ActiveSupport::TestCase
