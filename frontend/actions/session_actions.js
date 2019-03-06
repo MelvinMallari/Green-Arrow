ad888f0308f990271182e309e5901abfc72701b6
Melvin Mallari <mtmallari@ucdavis.edu>
2019-03-06 12:24:27 -0800

    Create users_reducer RECEIVE_USER_DATA case

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,7 +1,7 @@
 import * as SessionApiUtil from '../util/session_api_util';
 
 export const RECEIVE_USER = "RECEIVE_USER";
-export const RECEIVE_USER_INFO = "RECEIVE_USER_INFO";
+export const RECEIVE_USER_DATA = "RECEIVE_USER_DATA";
 export const LOGOUT_USER = "LOGOUT_USER";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
@@ -12,7 +12,7 @@ const receiveUser = user => ({
 })
 
 const receiveUserData = userData => ({
-  type: RECEIVE_USER_INFO,
+  type: RECEIVE_USER_DATA,
   userData,
 });
 
--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,11 +1,14 @@
-import {RECEIVE_USER} from '../actions/session_actions';
+import {RECEIVE_USER, RECEIVE_USER_DATA} from '../actions/session_actions';
 import merge from 'lodash/merge';
 
 const usersReducer = (state = {}, action) => {
   Object.freeze(state);
+
   switch (action.type) {
     case RECEIVE_USER:
       return merge({}, state, { [action.user.id] : action.user });
+    case RECEIVE_USER_DATA:
+      return merge({}, state, { [action.userData.id] : action.userData });
     default:
       return state;
   }
