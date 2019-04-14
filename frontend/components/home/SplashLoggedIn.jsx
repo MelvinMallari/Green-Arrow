756fe40821119d308d080347208f694b7bd97b59
Melvin Mallari <mtmallari@ucdavis.edu>
2019-04-13 21:21:21 -0700

    Add personal links

--- frontend/components/home/SplashLoggedIn.jsx
+++ frontend/components/home/SplashLoggedIn.jsx
@@ -116,6 +116,21 @@ class SplashLoggedIn extends React.Component {
                 currentUser={currentUser}
                 stocks={stocks} />
           </main>
+          <footer className="splash-footer">
+            <div className="personal-links-container">
+              <div className="personal-links">
+                <a href="https://github.com/MelvinMallari/Green-Arrow">
+                  <i class="fab fa-github-square fa-3x fa-icon"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/melvinmallari/">
+                  <i class="fab fa-linkedin fa-3x fa-icon"></i>
+                </a>
+                <a href="https://melvinmallari.com/">
+                  <i class="fas fa-address-card fa-3x fa-icon"></i>
+                </a>  
+              </div>
+            </div>
+          </footer>
         </div>
       );
     }
--- frontend/components/home/SplashLoggedOut.jsx
+++ frontend/components/home/SplashLoggedOut.jsx
@@ -33,6 +33,21 @@ class SplashLoggedOut extends React.Component {
             </section>
           </div>
         </div>
+        <footer className="splash-footer">
+          <div className="personal-links-container">
+            <div className="personal-links">
+              <a href="https://github.com/MelvinMallari/Green-Arrow">
+                <i class="fab fa-github-square fa-3x fa-icon"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/melvinmallari/">
+                <i class="fab fa-linkedin fa-3x fa-icon"></i>
+              </a>
+              <a href="https://melvinmallari.com/">
+                <i class="fas fa-address-card fa-3x fa-icon"></i>
+              </a>  
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
