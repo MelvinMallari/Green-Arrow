738de962b5c6ceabe63450d0617b8a63a45a2b50
Melvin Mallari <mtmallari@ucdavis.edu>
2019-05-22 20:21:02 -0700

    Switch API to WTD

--- frontend/components/chart/StockChart.jsx
+++ frontend/components/chart/StockChart.jsx
@@ -1,14 +1,7 @@
-import React from 'react'
-import {
-  LineChart, 
-  Line, 
-  XAxis, 
-  YAxis, 
-  CartesianGrid, 
-  Tooltip, 
-} from 'recharts';
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 import { formatMoney } from '../../util/util.js';
-import ToolTip from '../chart/ToolTip';
+import ToolTip from "./ToolTip";
 
 const INTERVAL_TO_AMOUNT_DATAPOINTS = {
   '5Y': 1258,
@@ -16,11 +9,10 @@ const INTERVAL_TO_AMOUNT_DATAPOINTS = {
   '3M': 64,
   '1M': 24,
   '1W': 5,
-  '1D': 390
+  '1D': 390,
 };
 
 class StockChart extends React.Component {
-
   calcDomain(data) {
     return [Math.min(...data), Math.max(...data)];
   }
@@ -30,16 +22,16 @@ class StockChart extends React.Component {
   }
 
   findReference(data) {
-    let values = Object.values(data);
+    const values = Object.values(data);
     for (let i = 0; i < data.length - 1; i++) {
-      let reference = values[i]
+      const reference = values[i];
       if (reference) return reference.close;
     }
     return 0;
   }
 
   calcInitPrice(stock) {
-    const stockIntradayData = stock.stockIntradayData;
+    const {stockIntradayData} = stock;
     let currentPrice;
 
     if (!stockIntradayData.length) {
@@ -47,7 +39,9 @@ class StockChart extends React.Component {
       currentPrice = 0;
     } else {
       let i = stockIntradayData.length - 1;
-      while (!stockIntradayData[i].close) { i--; }
+      while (!stockIntradayData[i].close) {
+        i--;
+      }
       currentPrice = stockIntradayData[i].close.toFixed(2);
     }
     const initPrice = parseFloat(currentPrice);
@@ -55,10 +49,10 @@ class StockChart extends React.Component {
   }
 
   initialStockData(stock, reference) {
-    const companyName = stock.companyName;
+    const {companyName} = stock;
     const initPrice = this.calcInitPrice(stock);
     const priceDifferential = parseFloat((initPrice - reference).toFixed(2));
-    const pctDifferential = ((initPrice - reference) / reference * 100).toFixed(2);
+    const pctDifferential = (((initPrice - reference) / reference) * 100).toFixed(2);
     return [companyName, formatMoney(initPrice), formatMoney(priceDifferential), pctDifferential];
   }
 
@@ -75,18 +69,18 @@ class StockChart extends React.Component {
       return data.filter((stock, i) => { 
         if (i % 5 === 0 && stock.close) return true;
       });
-    } else {
+    } 
       data = stock.stockData.slice(0);
       let end = this.calcEndIndex(data, range);
       return data.reverse().slice(0, end).reverse();
-    }
+    
   }
 
   renderThemeChanges(initPctDiff) {
     if (initPctDiff < 0) {
-     document.body.classList.add('red-theme');
+      document.body.classList.add('red-theme');
     } else {
-     document.body.classList.remove('red-theme');
+      document.body.classList.remove('red-theme');
     }
   }
 
@@ -99,46 +93,35 @@ class StockChart extends React.Component {
     const theme = initPctDiff < 0 ? '#f45531' : '#21ce99';
 
     this.renderThemeChanges(initPctDiff);
-    return(
+    return (
       <div>
         <header className="stock-info">
           <h1 className="company-name">{companyName}</h1>
-          <div><span id="price">{initPrice}</span></div>
+          <div>
+            <span id="price">{initPrice}</span>
+          </div>
           <div className="price-diff">
-            <span id="price-diff" className="diff"> {initPriceDiff}</span>
-            <span id="pct-diff" className="diff pct-diff">({initPctDiff})%</span>
+            <span id="price-diff" className="diff">
+              {' '}
+              {initPriceDiff}
+            </span>
+            <span id="pct-diff" className="diff pct-diff">
+              ({initPctDiff})%
+            </span>
           </div>
         </header>
-        <LineChart
-          width={676}
-          height={196}
-          data={data}
-          margin={{ top: 0, right: 0, left: 0, bottom: 0, }} >
-          <CartesianGrid 
-            strokeDasharray="3 3" 
-            horizontal={false} 
-            vertical={false} />
-          <XAxis 
-            dataKey="date" 
-            hide={true} />
-          <YAxis 
-            hide={true}
-            dataKey="close" 
-            domain={this.calcDomain(data)} />
-          <Tooltip  
-            content={<ToolTip 
-                      interval={this.props.interval} 
-                      diffReference={diffReference} />}
+        <LineChart width={676} height={196} data={data} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
+          <CartesianGrid strokeDasharray="3 3" horizontal={false} vertical={false} />
+          <XAxis dataKey="date" hide />
+          <YAxis hide={true} dataKey="close" domain={this.calcDomain(data)} />
+          <Tooltip
+            content={<ToolTip interval={this.props.interval} diffReference={diffReference} />}
             isAnimationActive={false}
             offset={-37}
-            position={{y: -20}} />
-          <Line 
-            animationDuration={850} 
-            dataKey="close" 
-            stroke={theme} 
-            dot={false} 
-            strokeWidth={2}/>
-       </LineChart>
+            position={{ y: -20 }}
+          />
+          <Line animationDuration={850} dataKey="close" stroke={theme} dot={false} strokeWidth={2} />
+        </LineChart>
       </div>
     );
   }
--- frontend/util/stock_api_util.js
+++ frontend/util/stock_api_util.js
@@ -1,41 +1,35 @@
-export const fetchStock = symbol => (
+export const fetchStock = symbol =>
   $.ajax({
-    method: "GET",
-    url: `api/stocks/${symbol}`
-  })
-);
+    method: 'GET',
+    url: `api/stocks/${symbol}`,
+  });
 
-export const fetchStocks = () => (
+export const fetchStocks = () =>
   $.ajax({
-    method: "GET",
-    url: `api/stocks`
-  })
-);
+    method: 'GET',
+    url: `api/stocks`,
+  });
 
-export const fetchStockData = symbol => (
+export const fetchStockData = symbol =>
   $.ajax({
-    method: "GET",
-    url: `https://api.iextrading.com/1.0/stock/${symbol}/chart/5Y/?filter=close,date,volume`
-  })
-);
+    method: 'GET',
+    url: `https://www.worldtradingdata.com/api/v1/history?symbol=${symbol}&sort=newest&api_token=3byQgXtIwUX4TsXM5ph7P2owTntgiAlEpNexNgDaeNVISVmxyIGJbPKXA7UA`,
+  });
 
-export const fetchStockIntradayData = symbol => (
+export const fetchStockIntradayData = symbol =>
   $.ajax({
-    method: "GET",
-    url: `https://api.iextrading.com/1.0/stock/${symbol}/chart/1D?filter=close,label`
-  })
-);
+    method: 'GET',
+    url: `https://api.iextrading.com/1.0/stock/${symbol}/chart/1D?filter=close,label`,
+  });
 
-export const fetchStockInfo = symbol => (
+export const fetchStockInfo = symbol =>
   $.ajax({
-    method: "GET",
-    url: `https://api.iextrading.com/1.0/stock/${symbol}/batch?types=stats,company`
-  })
-);
+    method: 'GET',
+    url: `https://api.iextrading.com/1.0/stock/${symbol}/batch?types=stats,company`,
+  });
 
-export const fetchStockNews = symbol => (
+export const fetchStockNews = symbol =>
   $.ajax({
-    method: "GET",
+    method: 'GET',
     url: `https://newsapi.org/v2/everything?q=${symbol}&sortBy=relevancy&pageSize=5&language=en&domains=yahoo.com,fool.com,forbes.com&apiKey=b20ba9a311bf4fa6b83c4f00e739bb02`,
-  })
-);
+  });
